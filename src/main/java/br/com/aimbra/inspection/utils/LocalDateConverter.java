8d2cbf5650fd7739892d08649f167083a1116e1a
Thiago Cunha <grupocodenome@gmail.com>
2019-02-15 00:43:06 -0200

    Add localdate converter

---
+++ src/main/java/br/com/aimbra/inspection/utils/LocalDateConverter.java
@@ -0,0 +1,23 @@
+package br.com.aimbra.inspection.utils;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.Date;
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter(autoApply = true)
+public class LocalDateConverter implements AttributeConverter<LocalDate, Date> {
+
+    @Override
+    public Date convertToDatabaseColumn(LocalDate date) {
+        Instant instant = Instant.from(date);
+        return Date.from(instant);
+    }
+
+    @Override
+    public LocalDate convertToEntityAttribute(Date value) {
+        Instant instant = value.toInstant();
+        return LocalDate.from(instant);
+    }
+}

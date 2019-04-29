7cf872c26ea9c8af56e0c11fb9d20b6f35fcbd1e
Thiago Cunha <thiagosantoscunha@outlook.com>
2019-04-29 18:01:07 -0300

    Dados fake da aplicação

---
+++ _BD/projeto_web_db_public_tb_estado.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.tb_estado (id, nome, uf) VALUES (1, 'Minas Gerais', 'MG');
+INSERT INTO public.tb_estado (id, nome, uf) VALUES (2, 'Bahia', 'BA');
+INSERT INTO public.tb_estado (id, nome, uf) VALUES (3, 'São Paulo', 'SP');
+INSERT INTO public.tb_estado (id, nome, uf) VALUES (4, 'Espirito Santo', 'ES');
+INSERT INTO public.tb_estado (id, nome, uf) VALUES (5, 'Rio de Janeiro', 'RJ');
+INSERT INTO public.tb_estado (id, nome, uf) VALUES (6, 'Tocantins', 'TO');
+INSERT INTO public.tb_estado (id, nome, uf) VALUES (7, 'Paraná', 'PA');
+INSERT INTO public.tb_estado (id, nome, uf) VALUES (8, 'Amapa', 'AP');
